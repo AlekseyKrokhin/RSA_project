a5a16863732bdaedcd8383ae0ff09254abd5a43f
Алексей Крохин <90411937+AlekseyKrokhin@users.noreply.github.com>
2021-11-15 19:06:42 +0300

    Update test.py

--- test.py
+++ test.py
@@ -11,30 +11,9 @@ from main import Generate_prime_number
 
 class UnitTest(unittest.TestCase):
 
-    def test_coprime(self):
-        self.assertEqual(CoprimeTest(13, 3), True)
-        self.assertEqual(CoprimeTest(15, 5), False)
-        self.assertEqual(CoprimeTest(92, 4), False)
-        self.assertEqual(CoprimeTest(17, 3), True)
-
-    def test_D(self):
-        self.assertEqual(GenerateD_e(5, 192), 77)
-        self.assertEqual(GenerateD_e(17, 231), 68)
-        self.assertEqual(GenerateD_e(491, 979), 327)
-
     def test_encr(self):
         main.n, main.e, main.D, main.p, main.q = keygen()
         self.assertEqual(Decrypt(Encrypt('Привет')), 'Привет')
         self.assertEqual(Decrypt(Encrypt('AbCdEfАбВгД')), 'AbCdEfАбВгД')
         self.assertEqual(Decrypt(Encrypt('!"№%:,.;@#$%^&*∂å∂ƒ√')), '!"№%:,.;@#$%^&*∂å∂ƒ√')
         self.assertEqual(Decrypt(Encrypt('Фаптш FSF3415__₽')), 'Фаптш FSF3415__₽')
-
-    def test_isprime(self):
-        self.assertEqual(isPrime(77), False)
-        self.assertEqual(isPrime(7), True)
-        self.assertEqual(isPrime(15), False)
-        self.assertEqual(isPrime(881), True)
-        self.assertEqual(isPrime(1000000), False)
-        self.assertEqual(isPrime(2), True)
-        self.assertEqual(isPrime(1), False)
-        self.assertEqual(isPrime(999983), True)
