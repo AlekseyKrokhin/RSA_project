617616931f338bb04da368cc97a573b666558ef5
Aleksey Krokhin <askrokhin@miem.hse.ru>
2021-11-19 16:05:17 +0300

    Цифровая подпись

--- main.py
+++ main.py
@@ -65,17 +65,21 @@ def Euler(num1, num2):
     """
     return (num1 - 1)*(num2 - 1)
 
-def GenerateD_e(phi):
-    """
-
-    :param phi: the value of the Euler function
-    :return: secret (D) and open(e) exponents
-    """
+def GenerateE(phi):
     e = random.randint(1000000000, 100000000000)
     while not CoprimeTest(phi, e):
         e = random.randint(1000000000, 100000000000)
-    D = pow(e, -1, phi)
-    return D, e
+    return e
+
+
+def GenerateD(phi, e):
+    """
+    :param phi: the value of the Euler function
+    :return: secret (D) and open(e) exponents
+    """
+    return pow(e, -1, phi)
+
+print(GenerateD(1111, 16))
 
 def keygen():
     """
@@ -85,13 +89,13 @@ def keygen():
     p, q = Generate_prime_number(), Generate_prime_number()
     n = p * q
     phi = Euler(p, q)
-    D, e = GenerateD_e(phi)
-    print('Открытый ключ (e, n): \n', 'e = ', e, '\n'
+    e = GenerateE(phi)
+    D = GenerateD(phi, e)
+    print('Открытый ключ (e, n): \ne = ', e, '\n'
           'n = ', n, '\n'
-          'Закрытый ключ (d, n): e = ', e, '\n'
+          'Закрытый ключ (d, n): \n'
           'D = ', D, '\n'
-          'p = ', p, '\n'
-          'q = ', q, '\n'
+          'n = ', n, '\n'
           )
     return n, e, D, p, q
 
@@ -112,15 +116,15 @@ def Encrypt(message):
     emessage = ""
     for i in message:
         i = ord(i)
-        i = '{0:011b}'.format(i)
+        i = '{0:013b}'.format(i)
         emessage += i
     emessage = "1" + emessage
     emessage = pow(int(emessage), e, n)
+
     return emessage
 
 def Decrypt(emessage):
     emessage = str(pow(int(emessage), D, n))
-    print(emessage)
     emessage = emessage[1:]
     count = 0
     dmessage = ''
@@ -128,16 +132,43 @@ def Decrypt(emessage):
     for i in emessage:
         count += 1
         num += i
-        if count == 11:
+        if count == 13:
             dmessage += chr((sec_to_dec(num)))
             count = 0
             num = ''
+
     return dmessage
+
+def creat_electronic_signature(message):
+    signature = ""
+    for i in message:
+        i = ord(i)
+        i = '{0:013b}'.format(i)
+        signature += i
+    signature = "1" + signature
+    signature = pow(int(signature), D, n)
+    return signature
+
+def verif_electronic_signature(signature, message):
+    signature = str(pow(int(signature), D, n))
+    signature = signature[1:]
+    count = 0
+    message_ = ''
+    num = ''
+    for i in signature:
+        count += 1
+        num += i
+        if count == 13:
+            message_ += chr((sec_to_dec(num)))
+            count = 0
+            num = ''
+    if message_ == message:
+        return "Verification successful!"
 
 if __name__ == '__main__':
     while True:
         order = int(input(
-            "Что вы хотите сделать? \nЗашифровать сообщение (1 на клавиатуре), Расшифровать (2 на клавиатуре) или Сгенерировать ключи (3 на клавиатуре): \n"))
+            "Что вы хотите сделать? \nЗашифровать сообщение (1 на клавиатуре),\nРасшифровать (2 на клавиатуре),\nСгенерировать ключи (3 на клавиатуре),\nИспользовать электронную подпись (4 на клавиатуре): \n"))
         if order == 1:
             message = str(input('Введите исходное сообщение М: '))
             order1 = int(input('Какие ключи использовать? \n Автоматически сгенерированные (1 на клавиатуре), собственные ключи (2 на клавиатуре), ключи из файла(3 на клавиатуре): \n'))
@@ -167,11 +198,14 @@ if __name__ == '__main__':
         if order == 3:
             print("Получаю ключи...")
             keygen()
-            '''print('Открытый ключ (e, n): e = ', e, '\n'
-                      'n = ', n, '\n'
-                      'Закрытый ключ (d, n): e = ', e, '\n'
-                      'D = ', D, '\n'
-                      'p = ', p, '\n'
-                      'q = ', q, '\n'
-                      )'''
+
+        if order == 4:
+            order2 = int(input("Создать подпись (1 на клавитауре) или Проверить подпись (2 на клавиатуре)? \n"))
+            if order2 == 1:
+                message = str(input('Введите сообщение М: '))
+                n, e, D, p, q = keygen()
+                print('Ваша электронная подпись S: ', creat_electronic_signature(message))
+
+
+
 
