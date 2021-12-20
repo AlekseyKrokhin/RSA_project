6fb072760f2ee555a1323b0f90a7188d77fe97ea
Aleksey Krokhin <askrokhin@miem.hse.ru>
2021-12-20 15:36:04 +0300

    final

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ RANGE_E = 100000000000
 def MillerRabin(p, repeat = 5):
     """
 
-    Probability test for simplicity
+    Probability test for simplicity:
     https://ru.wikipedia.org/wiki/%D0%A2%D0%B5%D1%81%D1%82_%D0%9C%D0%B8%D0%BB%D0%BB%D0%B5%D1%80%D0%B0_%E2%80%94_%D0%A0%D0%B0%D0%B1%D0%B8%D0%BD%D0%B0
 
     :param p: prime number
